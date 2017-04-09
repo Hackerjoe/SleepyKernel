e62b024ee8cc498ae0eb4f9b870774a8231cd189
Hackerjoe <jspankin5@gmail.com>
2017-04-09 11:38:38 -0600

    readtest

---
+++ sleepyReadTest.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+int main(int argc, char* argv[])
+{
+  int fd;
+  fd = open("/dev/sleepy9", O_RDONLY);
+  if(fd < 0)
+  {
+    printf("Unable to open\n");
+    return -1;
+  }
+  char b[1];
+  while(1)
+  {
+    write(fd,&sleepNumber,4);
+    if(read(fd,b,1) <0)
+    {
+      printf("ERROR while reading.\n");
+    }
+    else
+    {
+      printf("%d\n",b[0]);
+    }
+  }
+  if(ret < 0)
+  {
+    printf("Could Not Write\n");
+  }
+  return 0;
+}
